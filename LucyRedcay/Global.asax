6101a3365a233e083702291affbc211dc09b62e8
Phil Deren <phil.deren@williams-forrest.com>
2014-03-09 16:32:30 -0400

    Initial Load

---
+++ LucyRedcay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LucyRedcay.MvcApplication" Language="C#" %>
---
+++ LucyRedcay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LucyRedcay.Startup))]
+namespace LucyRedcay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
